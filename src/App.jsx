79c0a38d2a3c6501a2987d1e963fb27d35c3767f
NinoTraverso <njt98git@proton.me>
2023-11-28 20:35:31 +0000

    further react practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import Navbar from "./components/Navbar";
+
+let x = "HelloReact!";
+
+let array = ["user1", "user2", "user3", "user4", "user5"];
+
+let data = "man";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+      {/* EXAMPLE 1 */}
+      {x}
+
+      {/* EXAMPLE 2 */}
+      {array.map((user) => {
+        return <h2 key={user}>{user}</h2>;
+      })}
+
+      {/* EXAMPLE 3 */}
+      {data === "man" ? <h1>Man</h1> : <h1>Woman</h1>}
+    </div>
+  );
 };
 
 export default App;
