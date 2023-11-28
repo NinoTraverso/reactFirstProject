12f777a4243bda035d01fb29d4c387d50feff290
NinoTraverso <njt98git@proton.me>
2023-11-28 21:35:54 +0000

    further react excercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 
 let x = "HelloReact!";
@@ -6,7 +7,22 @@ let array = ["user1", "user2", "user3", "user4", "user5"];
 
 let data = "man";
 
+let y = 0;
+
 const App = () => {
+  const [z, setz] = useState(0);
+
+  const btnClick = () => {
+    console.log("Click");
+
+    setz(z + 1);
+
+    y++;
+
+    console.log("y =", y);
+    console.log("z =", z);
+  };
+
   return (
     <div>
       <Navbar />
@@ -20,6 +36,17 @@ const App = () => {
 
       {/* EXAMPLE 3 */}
       {data === "man" ? <h1>Man</h1> : <h1>Woman</h1>}
+
+      {/* EXAMPLE 4*/}
+
+      {z}
+      <button
+        onClick={() => {
+          btnClick();
+        }}
+      >
+        Click Me
+      </button>
     </div>
   );
 };
